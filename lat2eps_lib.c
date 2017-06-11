fbac15b2c4c40b924d14e281c53a54addfd7fc3c
André de la Rocha <47720370+adlroc@users.noreply.github.com>
2017-06-11 05:50:47 -0300

    text

--- lat2eps_lib.c
+++ lat2eps_lib.c
@@ -13,15 +13,23 @@
 #include "lat2eps_lib.h"
 
 
-#define MAXL   4096   // Maximum linear dimension of the lattice.
-#define MAXQ   16     // Maximum number of different site colors.
-
-
-static int lattice[MAXL][MAXL];
-static int palette[MAXQ] = { 0xFFFFFF, 0x000000, 0xBE2633, 0x44891A, 0x005784, 0xF7E26B, 0xA46422, 0xB2DCEF, 0xEB8931, 0x1B2632, 0xE06F8B, 0x493C2B, 0x2F484E, 0x9D9D9D, 0x31A2F2, 0xA3CE27 };
+static int lattice[LAT2EPS_MAXH][LAT2EPS_MAXW];
+static int palette[LAT2EPS_MAXQ] = { 0xFFFFFF, 0x000000, 0xBE2633, 0x44891A, 0x005784, 0xF7E26B, 0xA46422, 0xB2DCEF, 0xEB8931, 0x1B2632, 0xE06F8B, 0x493C2B, 0x2F484E, 0x9D9D9D, 0x31A2F2, 0xA3CE27 };
 
 static int width;
 static int height;
+static int txtcnt;
+
+static struct {
+	float x;
+	float y;
+	float ax;
+	float ay;
+	float angle;
+	int size;
+	int color;
+	const char *text;
+} textinfo[LAT2EPS_MAXT];
 
 
 static void gen_eps_prolog(FILE *f, int width, int height, int scale, int border);
@@ -34,16 +42,20 @@ int lat2eps_init(int w, int h)
 {
 	int x, y;
 
-	if ((w <= 0) || (w > MAXL) || (h <= 0) || (h > MAXL)) {
+	if ((w <= 0) || (w > LAT2EPS_MAXW) || (h <= 0) || (h > LAT2EPS_MAXH)) {
 		return 0;
 	}
+	
+	txtcnt = 0;
 
 	width = w;
 	height = h;
 
-	for (y = 0; y < height; ++y)
-		for (x = 0; x < width; ++x)
+	for (y = 0; y < height; ++y) {
+		for (x = 0; x < width; ++x) {
 			lattice[y][x] = 0;
+		}
+	}
 
 	return 1;
 }
@@ -61,8 +73,25 @@ void lat2eps_set_site(int x, int y, int s)
 // Sets a color index to a palette entry in the 0xRRGGBB format.
 void lat2eps_set_color(int index, int pal)
 {
-	if ((index >= 0) && (index < MAXQ)) {
+	if ((index >= 0) && (index < LAT2EPS_MAXQ)) {
 		palette[index] = pal;
+	}
+}
+
+
+// Adds a text entry
+void lat2eps_text_out(float x, float y, float ax, float ay, float angle, int size, int color, const char *text)
+{
+	if ((txtcnt < LAT2EPS_MAXT) && (color >= 0) && (color < LAT2EPS_MAXQ) && text) {
+		textinfo[txtcnt].x = x;
+		textinfo[txtcnt].y = y;
+		textinfo[txtcnt].ax = ax;
+		textinfo[txtcnt].ay = ay;
+		textinfo[txtcnt].angle = angle;
+		textinfo[txtcnt].size = size;
+		textinfo[txtcnt].color = color;
+		textinfo[txtcnt].text = text;
+		txtcnt++;
 	}
 }
 
@@ -72,7 +101,7 @@ int lat2eps_gen_eps(const char *filename, int border, int scale)
 {
 	FILE *f;
 
-	if ((width <= 0) || (width > MAXL) || (height <= 0) || (height > MAXL) || (border < 0) || (scale <= 0)) {
+	if ((width <= 0) || (width > LAT2EPS_MAXW) || (height <= 0) || (height > LAT2EPS_MAXH) || (border < 0) || (scale <= 0)) {
 		return 0;
 	}
 
@@ -90,7 +119,7 @@ int lat2eps_gen_eps(const char *filename, int border, int scale)
 }
 
 
-// Generates EPS prolog, including L/P procedures and palette definition.
+// Generates EPS prolog, including L/P/T procedures and palette definition.
 void gen_eps_prolog(FILE *f, int width, int height, int scale, int border)
 {
 	int i;
@@ -101,8 +130,15 @@ void gen_eps_prolog(FILE *f, int width, int height, int scale, int border)
 	fprintf(f, "%%%%BeginProlog\n");
 	fprintf(f, "/L { 2 rectfill } def\n");    // Line procedure. The extra row is for adding overlap between lines, to work around an anti-aliasing bug in several ps/pdf viewers that would show glitches otherwise.
 	fprintf(f, "/P { 1 2 rectfill } def\n");  // Pixel procedure.
-	for (i = 0; i < MAXQ; ++i)
+	// Text procedure
+	fprintf(f, "/T { /SS exch def /SZ exch def /RR exch def /AY exch def /AX exch def /YY exch def /XX exch def\n");
+	fprintf(f, "/Times-Roman findfont SZ scalefont setfont gsave 1 -1 scale newpath XX YY neg moveto SS true charpath pathbbox exch 4 1 roll sub neg 3 1 roll sub\n");
+	fprintf(f, "/WW exch def /HH exch def XX WW RR cos mul AX mul sub HH RR sin mul 1 AY sub mul add\n");
+	fprintf(f, "YY neg WW RR sin mul AX mul sub HH RR cos mul 1 AY sub mul sub newpath moveto RR rotate SS show grestore } def\n");
+	// Palette
+	for (i = 0; i < LAT2EPS_MAXQ; ++i) {
 		fprintf(f, "/C%X { %f %f %f setrgbcolor } def\n", i, ((palette[i] >> 16) & 255)/255.0, ((palette[i] >> 8) & 255)/255.0, (palette[i] & 255)/255.0);
+	}
 	fprintf(f, "%%%%EndProlog\n");
 	fprintf(f, "%%%%Page: 1 1\n");
 	fprintf(f, "%d %d scale\n", scale, -scale);  // Inverts y axis to be top to bottom.
@@ -113,6 +149,13 @@ void gen_eps_prolog(FILE *f, int width, int height, int scale, int border)
 // Generates EPS epilog.
 void gen_eps_epilog(FILE *f, int width, int height, int scale, int border)
 {
+	int i;
+
+	// Outputs text entries
+	for (i = 0; i < txtcnt; ++i) {
+		fprintf(f, "C%X %f %f %f %f %f %d (%s) T\n", textinfo[i].color, textinfo[i].x, textinfo[i].y, textinfo[i].ax, textinfo[i].ay, textinfo[i].angle, textinfo[i].size, textinfo[i].text);
+	}
+	// Outputs border
 	if (border > 0) {
 		fprintf(f, "0 0 0 setrgbcolor\n");
 		fprintf(f, "%f setlinewidth\n", (float)border / scale);
@@ -137,7 +180,7 @@ void gen_eps_lattice(FILE *f, int width, int height)
 			while ((x + cnt < width) && (lattice[y][x + cnt] == s)) ++cnt;
 			
 			// Maps any positive or negative site value to one of the available colors.
-			col = (s >= 0) ? (s % MAXQ) : (MAXQ - 1 - (-s % MAXQ));
+			col = (s >= 0) ? (s % LAT2EPS_MAXQ) : (LAT2EPS_MAXQ - 1 - (-s % LAT2EPS_MAXQ));
 			
 			if (cnt > 1)
 				fprintf(f, "C%X %d %d %d L\n", col, x, y, cnt);
--- test.c
+++ test.c
@@ -1,11 +1,15 @@
+#include <math.h>
 #include "lat2eps_lib.h"
 
 #define L 256
+#define NC 250
 
 int main(void)
 {
-	int x, y;
+	int x, y, i;
 	
+	const char *s[] = { "T", "R", "E", "V", "A", " " };
+
 	lat2eps_init(L, L);
 
 	lat2eps_set_color(0, 0x606090);
@@ -13,7 +17,17 @@ int main(void)
 
 	for (y = 0; y < L; ++y)
 		for (x = 0; x < L; ++x)
-			lat2eps_set_site(x, y, y==0||y==L-1||x==0||x==L-1||x==y||x==L-1-y?1:0);
+			lat2eps_set_site(x, y, y==0||y==L-1||x==0||x==L-1||x==y||x==L-1-y?5:0);
+
+
+	for (i = 0; i < NC; ++i) {
+		float rt = (NC-i) / (float)NC;
+		float r = 5 + 150 * pow(rt, 0.8);
+		float ag = pow(rt, 0.6) * 360*8;
+		float ar = (ag+90)/180.0*M_PI;
+		float sz = 12 + 20 * rt;
+		lat2eps_text_out(L/2 + r * cos(ar), L/2 - r * sin(ar), 0.5, 0.5, ag, (int)sz, 1 + i % 15, s[i % 6]);
+	}
 
 	lat2eps_gen_eps("test.eps", 2, 2);
 
