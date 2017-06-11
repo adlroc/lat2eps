1284ca85e869f0ebc6d7709879d49ac631963f72
André de la Rocha <anroc.code@gmail.com>
2017-06-11 05:50:47 -0300

    text

--- aaa.c
+++ aaa.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	for (int y = 0; y < N; ++y)
 		for (int x = 0; x < N; ++x)
-			printf("%d %d %d\n", x, y, y==0||y==N-1||x==0||x==N-1||x==y?2:0);
+			printf("%d %d %d\n", x, y, y==0||y==N-1||x==0||x==N-1||x==y||x==N-y-1?2:0);
 
 	return 0;
 }
--- lat2eps_lib.h
+++ lat2eps_lib.h
@@ -6,8 +6,30 @@
  */
 
 
+#ifndef _LAT2EPS_H
+#define _LAT2EPS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+#define LAT2EPS_MAXW  4096   // Maximum lattice width
+#define LAT2EPS_MAXH  4096   // Maximum lattice height
+#define LAT2EPS_MAXQ    16   // Maximum number of different site colors
+#define LAT2EPS_MAXT  1024   // Maximum number of text entries
+
+
 int lat2eps_init(int w, int h);
 void lat2eps_set_site(int x, int y, int s);
 void lat2eps_set_color(int index, int pal);
+void lat2eps_text_out(float x, float y, float ax, float ay, float angle, int size, int color, const char *text);
 int lat2eps_gen_eps(const char *filename, int border, int scale);
+
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // _LAT2EPS_H
 
