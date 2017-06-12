60acff4ee574c9f49c60213126f4c6d5103f34ee
André de la Rocha <47720370+adlroc@users.noreply.github.com>
2017-06-11 23:04:23 -0300

    updated
    Signed-off-by: Andre de la Rocha <adlroc@gmail.com>

--- lat2eps_lib.h
+++ lat2eps_lib.h
@@ -1,6 +1,6 @@
 
 /*
- *  lat2eps lib 1.2
+ *  lat2eps lib 1.3
  *
  *  Coded by Andre de la Rocha. Public domain.
  */
@@ -11,25 +11,72 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif // __cplusplus
+#endif /* __cplusplus */
 
 
-#define LAT2EPS_MAXW  4096   // Maximum lattice width
-#define LAT2EPS_MAXH  4096   // Maximum lattice height
-#define LAT2EPS_MAXQ    16   // Maximum number of different site colors
-#define LAT2EPS_MAXT  1024   // Maximum number of text entries
+/**
+* Initializes the lattice resources.
+* @param maxw Maximum lattice width.
+* @param maxh Maximum lattice height.
+*/
+void lat2eps_init(unsigned int maxw, unsigned int maxh);
 
 
-int lat2eps_init(int w, int h);
-void lat2eps_set_site(int x, int y, int s);
-void lat2eps_set_color(int index, int pal);
-void lat2eps_text_out(float x, float y, float ax, float ay, float angle, int size, int color, const char *text);
-int lat2eps_gen_eps(const char *filename, int border, int scale);
+/**
+* Releases the lattice resources.
+*/
+void lat2eps_release();
+
+
+/**
+* Sets the value of a lattice site.
+* @param x Horizontal coordinate of the site.
+* @param y Vertical coordinate of the site.
+* @param s Value to set the site to.
+*/
+void lat2eps_set_site(unsigned int x, unsigned int y, int s);
+
+
+/**
+* Sets a color index to a palette entry in the 0xRRGGBB format.
+* @param index Color index.
+* @param pal   Palette entry in the 0xRRGGBB format
+*/
+void lat2eps_set_color(unsigned int index, unsigned int pal);
+
+
+/**
+* Adds a text entry.
+* @param x      X coordinate.
+* @param y      Y coordinate.
+* @param ax     Horizontal alignment. 0 for left-aligning the text relative to the X coordinate, 0.5 for centering it, 1 for right-aligning, etc.
+* @param ax     Vertical alignment. 0 for placing the top of the text on the Y coordinate, 0.5 for centering it, 1 for placing the bottom of the text, etc.
+* @param angle  Angle to rotate the text, in degrees (0 for horizontal, left to right text).
+* @param size   Font size.
+* @param color  Color index.
+* @param text   Text string.
+* @note         Must be called before lat2eps_gen_eps(). Parentheses must be escaped with backslashes.
+*/
+void lat2eps_text_out(float x, float y, float ax, float ay, float angle, unsigned int size, unsigned int color, const char *text);
+
+
+/**
+* Generates lattice graphic in EPS.
+* @param filename  Name of the EPS file that will be created, or NULL for outputting to stdout.
+* @param offx      Offset of the first lattice column that will be saved in the output.
+* @param offy      Offset of the first lattice row that will be saved in the output.
+* @param width     Width (in sites) of the sublattice that will be saved in the output
+* @param height    Height (in sites) of the sublattice that will be saved in the output
+* @param border    Width of a border that will be placed in the graphic (0 for no border).
+* @param scale     Scale that will be used while generating the graphic (e.g., using 2 will create a 2x2 pixel square for each site).
+* @return          Zero for failure, non-zero for success.
+*/
+int lat2eps_gen_eps(const char *filename, unsigned int offx, unsigned int offy, unsigned int width, unsigned int height, unsigned int border, unsigned int scale);
 
 
 #ifdef __cplusplus
 }
-#endif // __cplusplus
+#endif /* __cplusplus */
 
-#endif // _LAT2EPS_H
+#endif /* _LAT2EPS_H */
 
--- test.c
+++ test.c
@@ -1,24 +1,36 @@
+
+/*
+ *  Sample program using the lat2eps lib functions.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "lat2eps_lib.h"
 
-#define L 256
-#define NC 250
+
+#define L   256
+#define NC  250
+#define NL  6
+
 
 int main(void)
 {
 	int x, y, i;
 	
-	const char *s[] = { "T", "R", "E", "V", "A", " " };
+	const char *s[NL] = { "A", "B", "C", "D", "E", "F" };
 
 	lat2eps_init(L, L);
 
 	lat2eps_set_color(0, 0x606090);
 	lat2eps_set_color(1, 0x00ff00);
 
-	for (y = 0; y < L; ++y)
-		for (x = 0; x < L; ++x)
-			lat2eps_set_site(x, y, y==0||y==L-1||x==0||x==L-1||x==y||x==L-1-y?5:0);
-
+	for (y = 0; y < L; ++y) {
+		for (x = 0; x < L; ++x) {
+			lat2eps_set_site(x, y, y==0||y==L-1||x==0||x==L-1||x==y||x==L-1-y?5:pow(sin(y/80.0), 2)*pow(cos(x/80.0), 2)*15);
+		}
+	}
 
 	for (i = 0; i < NC; ++i) {
 		float rt = (NC-i) / (float)NC;
@@ -26,10 +38,11 @@ int main(void)
 		float ag = pow(rt, 0.6) * 360*8;
 		float ar = (ag+90)/180.0*M_PI;
 		float sz = 12 + 20 * rt;
-		lat2eps_text_out(L/2 + r * cos(ar), L/2 - r * sin(ar), 0.5, 0.5, ag, (int)sz, 1 + i % 15, s[i % 6]);
+		lat2eps_text_out(L/2 + r * cos(ar), L/2 - r * sin(ar), 0.5, 0.5, ag, (int)(sz+0.5), 1 + i % 15, s[i % NL]);
 	}
 
-	lat2eps_gen_eps("test.eps", 2, 2);
+	lat2eps_gen_eps("test.eps", 0, 0, L, L, 2, 2);
+	lat2eps_release();
 
 	return 0;	
 }
